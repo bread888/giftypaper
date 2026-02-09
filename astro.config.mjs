342128f002398818ce9bc63553d1fb00d61a38c0
Emanuele Sabia <e.sabia@qsinformatica.it>
2026-02-09 14:20:27 +0100

    fixed astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
-	site: "https://www.giftypaper.it",
-	integrations: [mdx(), sitemap()],
-	adapter: cloudflare({
-		platformProxy: { enabled: true },
-		},
-	}),
+  site: "https://www.giftypaper.it",
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 });
